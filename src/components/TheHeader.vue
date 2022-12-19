3d509fc0e398e2ed607756688766fc00b067fc9c
Dmitriy Vasilev <vdv0691@gmail.com>
2022-12-19 10:51:02 +0300

    feat: add app header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="header">
+        <div class="header__wrapper">
+            <div class="header__content container">
+                <TheNavigation/>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+import TheNavigation from './TheNavigation.vue';
+
+
+</script>
+
+<style scoped lang="scss">
+.header {
+    
+    &__wrapper {
+        @include flex-center;
+        width: 100%;
+        height: 100px;
+        background: #eee;
+    }
+}
+</style>
