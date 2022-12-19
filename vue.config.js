565a632c541ce68a7f62871528e404848af13dde
Dmitriy Vasilev <vdv0691@gmail.com>
2022-12-19 10:28:51 +0300

    feat: add global scss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/base.scss";
+                         @import "@/assets/scss/variables.scss";
+                         @import "@/assets/scss/mixins.scss";
+        `
+      },
+    },
+  }
 })
