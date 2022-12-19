b030c6e3180cf26a7276db4e6194aea9ba1786b0
Dmitriy Vasilev <vdv0691@gmail.com>
2022-12-19 10:42:51 +0300

    feat: add form fields components

---
+++ src/components/FormsFields/EmailField.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="form-fields">
+        <label for="email" class="label-field">E-mail</label>
+        <input id="email" 
+               class="input-field" 
+               :class="{error: errors.email}"
+               type="text" 
+               v-model="email"
+               @keyup="emailValidation"
+               @blur="emailValidation"
+               @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+        />
+        <div class="error-field">{{errors.email}}</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import useFormValidation from '@/helpers/FormValidation'
+
+const email = ref('')
+const { emailFieldValidation, errors } = useFormValidation()
+const emailValidation = () => {
+    emailFieldValidation('email', 'E-mail', email.value)
+}
+
+</script>
---
+++ src/components/FormsFields/IDField.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="form-fields">
+        <label for="companyid" class="label-field">ИНН</label>
+        <input id="companyid" 
+               class="input-field" 
+               :class="{error: errors.id}"
+               type="text" 
+               v-model="companyID" 
+               @keyup="idValidation"
+               @blur="idValidation"
+               @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+        />
+        <div class="error-field">{{errors.id}}</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import useFormValidation from '@/helpers/FormValidation'
+
+const companyID = ref('')
+const { idFieldValidation, errors } = useFormValidation()
+const idValidation = () => {
+    idFieldValidation('id', 'ИНН', companyID.value)
+}
+
+</script>
