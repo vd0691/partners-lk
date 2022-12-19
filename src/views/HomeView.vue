6277e192e3645698f8328bdd50719529c5d6a5d3
Dmitriy Vasilev <vdv0691@gmail.com>
2022-12-19 10:59:00 +0300

    feat: add home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <h1>Home Page</h1>  
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
 </script>
