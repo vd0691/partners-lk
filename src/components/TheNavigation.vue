e82f92db56bdd7d878201c23640ed3169827f66b
Dmitriy Vasilev <vdv0691@gmail.com>
2022-12-19 10:56:10 +0300

    feat: add app navigation

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="nav">
+        <ul class="nav__list">
+            <li class="nav__item">
+                <router-link class="nav__link" to="/">Главная</router-link>
+            </li>
+            <li class="nav__item">
+                <router-link class="nav__link" to="/orders">Заказы</router-link>
+            </li>
+            <li class="nav__item">
+                <router-link class="nav__link" to="/documents">Документы</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+.nav {
+
+    &__list {
+        @include flex-center;
+        list-style: none;
+    }
+
+    &__item {
+        margin: 0 10px;
+        text-transform: uppercase;
+    }
+}
+</style>
