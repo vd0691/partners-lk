70238d94e376576e537b119fa6c000950ba34390
Dmitriy Vasilev <vdv0691@gmail.com>
2022-12-22 12:38:53 +0300

    feat: create data fetching preloader store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import { createStore } from 'vuex'
 import auth from './modules/auth'
 import error from './modules/error'
 import products from './modules/products'
+import preloader from './modules/preloader'
 export default createStore({
   modules: {
     auth,
     error,
-    products    
+    products,
+    preloader 
   }
 })
