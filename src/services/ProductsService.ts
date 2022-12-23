8df7f099ffde50be7ce70f2f7a56458102bf5429
Dmitriy Vasilev <vdv0691@gmail.com>
2022-12-23 14:28:09 +0300

    feat: add route to login if response error 401

--- src/services/ProductsService.ts
+++ src/services/ProductsService.ts
@@ -1,18 +1,23 @@
 import { AuthorizationHeader } from "@/helpers/AuthorizationHeader"
-import axios from "axios"
+import router from "@/router"
+import axios, { AxiosError } from "axios"
 
 const API_URL = process.env.VUE_APP_API_URL
 
 export default function useProductsService() {
-    const getAllProducts =async () => {
+    const getAllProducts = async () => {
         try {
-            const allProducts = axios.get(`${API_URL}/items`, {
+            const allProducts = await axios.get(`${API_URL}/items`, {
                 headers: AuthorizationHeader()
             })
             return allProducts
 
         } catch (error) {
-            console.log(error)
+            const errors = error as AxiosError
+            if (errors.response?.status === 401) {
+                localStorage.removeItem('user')
+                router.push('/login')
+            }
         }
     }
 
