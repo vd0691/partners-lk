b52b85d96e1811278c3d773a302100a4164985f3
Dmitriy Vasilev <vdv0691@gmail.com>
2022-12-23 13:33:37 +0300

    feat: add interceptor for change loading status

---
+++ src/helpers/AxiosInterseptors.ts
@@ -0,0 +1,24 @@
+import axios from "axios"
+import { Store } from "vuex";
+
+interface State {
+    [key:string]: boolean
+}
+
+    export const loaderInterceptor = (store:Store<State>) => {
+        axios.interceptors.request.use(function (config) {
+            store.dispatch('getLoadingStatus', true)            
+            return config;
+          }, function (error) {
+            store.dispatch('getLoadingStatus', false)
+            return Promise.reject(error);
+        }); 
+        axios.interceptors.response.use(function (config) {
+            store.dispatch('getLoadingStatus', false)            
+            return config;
+          }, function (error) {
+            store.dispatch('getLoadingStatus', false)
+            return Promise.reject(error);
+        });        
+    }
+
