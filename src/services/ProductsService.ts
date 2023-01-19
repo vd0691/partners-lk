a9bb39ad63d4e6ca323c917fda24f6735e5bdfec
Dmitriy Vasilev <vdv0691@gmail.com>
2023-01-19 09:03:49 +0300

    feat: add required parameters for products fetching

--- src/services/ProductsService.ts
+++ src/services/ProductsService.ts
@@ -6,7 +6,7 @@ const API_URL = process.env.VUE_APP_API_URL
 export default function useProductsService() {
     const getProducts = async (id:string, from:number, size:number) => {
         try {
-            const allProducts = await axios.get(`${API_URL}/items`, {
+            const allProducts = await axios.get(`${API_URL}/items?partnerId=01e27f81-7aed-11e9-812e-00155d033601`, {
                     params: {
                     from: from || 0,
                     size: size || 20,
@@ -22,7 +22,7 @@ export default function useProductsService() {
 
     const getCategories = async () => {
         try {
-            const categories = await axios.get(`${API_URL}/items/groups`)
+            const categories = await axios.get(`${API_URL}/items/groups?size=2000`)
             return categories
         } catch (error) {
             console.log(error)
