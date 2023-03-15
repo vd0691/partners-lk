444cebd2c0e181c16d4a26eb8de1bc68f0bee3c1
Dmitriy Vasilev <vdv0691@gmail.com>
2023-03-15 07:02:25 +0300

    add emit for close modal outside click

--- src/components/ModalWindow.vue
+++ src/components/ModalWindow.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="modal">
+    <div class="modal" @click.self="$emit('closeWindow')">
         <div class="modal__content">
             <div class="closeBtn" @click="$emit('closeWindow')">
                 <svg data-v-716ea81c="" width="16" height="16" viewBox="0 0 14 14" fill="#667585" xmlns="http://www.w3.org/2000/svg"><path d="M.305.292a.999.999 0 011.413 0L6.006 4.58 10.295.292a.999.999 0 011.412 1.413L7.42 5.994l4.287 4.288a1 1 0 01.083 1.32l-.082.093a.999.999 0 01-1.413 0L6.006 7.407l-4.288 4.288a1 1 0 11-1.413-1.413l4.288-4.288L.305 1.705A.999.999 0 01.222.386L.305.292z"></path></svg>
