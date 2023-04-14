daaeb0387822dfb20100de3bc9233477b56c6784
Dmitriy Vasilev <vdv0691@gmail.com>
2023-04-14 07:26:34 +0300

    delete

--- src/interfaces/Contractor.ts
+++
@@ -1,9 +0,0 @@
-export interface Contractor {
-    id: string,
-    name: string,
-    description: string,
-    inn: string,
-    kpp: string,
-    legalAddress: string,
-    actualAddress: string
-}
