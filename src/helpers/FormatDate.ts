42c8e7d57185f640d600d86d2dd89c24ce066de6
Dmitriy Vasilev <vdv0691@gmail.com>
2023-04-18 11:09:19 +0300

    add condition for fullfilment

--- src/helpers/FormatDate.ts
+++ src/helpers/FormatDate.ts
@@ -1,7 +1,7 @@
 export const formatDate = (date: string) => {
     const time = date.split('T')[1]
     const orderDate = date.split('T')[0].replace(/^(\d+)-(\d+)-(\d+)$/, `$3.$2.$1`)
-    const orderTime = time.slice(0, -time.length + 5)
-    const currentDate = orderDate + ' ' + orderTime
-    return currentDate
+    const orderTime = time ? time.slice(0, -time.length + 5) : ''
+    const fullDate = orderDate + ' ' + orderTime
+    return {fullDate, orderTime, orderDate}
 }
