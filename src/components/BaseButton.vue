1535f75d1caa4b2e094e5b4ffad9d53e0ed33c2f
Dmitriy Vasilev <vdv0691@gmail.com>
2023-05-10 09:49:47 +0300

    add app base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <button class="button">{{ props.text }}</button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+</script>
